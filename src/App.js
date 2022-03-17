a184a71ecebd24ce37e4f834ee96c206395c8f0a
Waqar Khan <waqar.khan@techloyce.com>
2022-03-17 13:11:01 +0500

    form submit

--- src/App.js
+++ src/App.js
@@ -2,19 +2,30 @@ import { useState } from 'react';
 
 import User from './User';
 function App() {
-  const [show, setShow] = useState(true);
-  const [name, setName] = useState('a');
-  function changeName() {
-    setShow(!show);
-    setName('app');
+  const [name, setName] = useState('');
+  const [terms, setTerms] = useState(false);
+  const [intrest, setIntrest] = useState('');
+  function getFormData(e) {
+    console.log(name,terms,intrest);
+    e.preventDefault();
+    
   }
 
   return (
     <div className="App">
-      {
-        show ? <User name={name} /> : null
-      }
-        <button onClick={changeName}>Change!</button>
+        <form onSubmit={getFormData}>
+            <input type="text" placeholder='name' onChange={(e)=>setName(e.target.value)} /><br /><br />
+            <select onChange={(e)=>setIntrest(e.target.value)}>
+              <option>Select</option>
+              <option>Marvel</option>
+              <option>new</option>
+            </select>
+            <br /><br />
+            <input type="checkbox" onChange={(e)=>setTerms(e.target.checked)} /> Terms
+            <br /><br />
+            <button type="submit">Submit</button>
+            <button>Clear</button>
+        </form>
     </div>
   );
 }
